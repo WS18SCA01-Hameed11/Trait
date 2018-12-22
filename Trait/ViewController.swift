369021ff5fe8e0d19bd52e72fd80b66b2369a9a2
Hameed Abdullah <hameedabdullah@hameeds-macbook-pro.local>
2018-12-21 23:03:19 -0500

    Initial Commit

---
+++ Trait/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trait
+//
+//  Created by Hameed Abdullah on 12/21/18.
+//  Copyright © 2018 Hameed Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
